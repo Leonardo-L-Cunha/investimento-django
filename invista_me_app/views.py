16e714a187188b221faccc59dd0c77f2c2bb1c39
Leonardo <leonavidareal@gmail.com>
2024-05-27 01:56:24 -0300

    feat: como criar template em django

--- invista_me_app/views.py
+++ invista_me_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+def investimentos(request):
+    return HttpResponse('Vamos investir')
+
+
+def novo_investimento(request):
+    return render(request, 'investimentos/novo_investimento.html')
+
+def investimento_registrado(request):
+    investimento = {
+        'tipo_investimento': request.POST.get('tipoInvestimento')
+    }
+    return render(request, 'investimentos/investimento_registrado.html', investimento)
