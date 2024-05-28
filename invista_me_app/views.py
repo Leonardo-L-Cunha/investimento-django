aaf34955da7b4a6e27682e6f005c14a0481491fc
Leonardo <leonavidareal@gmail.com>
2024-05-28 00:49:28 -0300

    feat: listando investimentos

--- invista_me_app/views.py
+++ invista_me_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Investimento
+
+
 
 def investimentos(request):
-    return HttpResponse('Vamos investir')
+    dados = {
+        'dados' : Investimento.objects.all()
+    }
+    return render(request,'investimentos/investimentos.html', context=dados)
 
 
 def novo_investimento(request):
     return render(request, 'investimentos/novo_investimento.html')
-
-def investimento_registrado(request):
-    investimento = {
-        'tipo_investimento': request.POST.get('tipoInvestimento')
-    }
-    return render(request, 'investimentos/investimento_registrado.html', investimento)
