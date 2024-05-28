db358b500b98c5ef977df566c754a88c132d63ee
Leonardo <leonavidareal@gmail.com>
2024-05-28 01:14:58 -0300

    feat: criar investimento

--- invista_me_app/views.py
+++ invista_me_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect 
 from .models import Investimento
+from .forms import InvestimentoForm
 
 
 
@@ -11,11 +11,24 @@ def investimentos(request):
     return render(request,'investimentos/investimentos.html', context=dados)
 
 
-def novo_investimento(request):
-    return render(request, 'investimentos/novo_investimento.html')
 
 def detalhe(request, id_investimento):
     dados = {
         'dados': Investimento.objects.get(pk=id_investimento)
     }
     return render(request,'investimentos/detalhe.html', context=dados)
+
+
+def criar(request):
+    if request.method == 'POST':
+        investimento_form = InvestimentoForm(request.POST)
+        if investimento_form.is_valid():
+            investimento_form.save()
+        return redirect('investimentos')
+    else:
+        investimento_form = InvestimentoForm()
+        formulario = {
+            'formulario': investimento_form
+        }
+
+        return render(request, 'investimentos/novo_investimento.html', context=formulario)
